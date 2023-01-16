f245c3c2299476876acb1d8e895f33311c4bebed
HeistxBerlin <yasir.mansoori000@gmail.com>
2023-01-16 21:03:20 +0530

    update

--- assets/vendor/php-email-form/php-email-form.php
+++ assets/vendor/php-email-form/php-email-form.php
@@ -1,6 +1,6 @@
 <?php 
 $errors = '';
-$myemail = 'yourname@website.com';//<-----Put Your email address here.
+$myemail = 'yasir.mansoori000@gmail.com';//<-----Put Your email address here.
 if(empty($_POST['name'])  || 
    empty($_POST['email']) || 
    empty($_POST['message']))
